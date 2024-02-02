7e58bd3a3b06de23b5906e89a3605d5a2afbcaf6
Dingkun Yang <dingkun.yang@duke.edu>
2024-02-02 03:28:27 -0500

    add path back

--- config/website.js
+++ config/website.js
@@ -1,7 +1,7 @@
 const tailwind = require('../tailwind');
 
 module.exports = {
-  // pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
   siteTitle: 'Dingkun - About', // Navigation and Site Title
   siteTitleAlt: 'Yer1k', // Alternative Site title for SEO
